ea2f31052684c8f44cf78e5ee25e325f9422e391
Satrya Anas <catur.satrya@gmail.com>
2019-01-28 17:02:34 +0800

    check if exist verification

--- VolcomStockOpname/FormVerStockTakeNew.vb
+++ VolcomStockOpname/FormVerStockTakeNew.vb
@@ -17,6 +17,17 @@
     Private Sub BtnCreate_Click(sender As Object, e As EventArgs) Handles BtnCreate.Click
         If GVScan.RowCount > 0 And GVScan.FocusedRowHandle >= 0 Then
             Cursor = Cursors.WaitCursor
+
+            'check exist
+            Dim stake As New ClassStockTake()
+            Dim querycek As String = stake.queryVerTransMain("AND st.is_combine=2 AND st.id_st_trans='" + GVScan.GetFocusedRowCellValue("id_st_trans").ToString + "' AND st.id_report_status!=5 ", "1")
+            Dim datacek As DataTable = execute_query(querycek, -1, True, "", "", "", "")
+            If datacek.Rows.Count > 0 Then
+                Cursor = Cursors.Default
+                stopCustom("This number already verified")
+                Exit Sub
+            End If
+
             Dim query As String = "INSERT INTO tb_st_trans_ver (id_st_trans,id_wh_drawer, st_trans_ver_number, remark, st_trans_ver_date, st_trans_ver_by, is_combine) 
             VALUES ('" + GVScan.GetFocusedRowCellValue("id_st_trans").ToString + "','" + GVScan.GetFocusedRowCellValue("id_wh_drawer").ToString + "', '" + header_number("6") + "', '" + addSlashes(TxtRemark.Text.ToString) + "', NOW(), '" + id_user + "', 2); SELECT LAST_INSERT_ID(); "
             Dim id_new As String = execute_query(query, 0, True, "", "", "", "")

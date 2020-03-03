3d97fa91d50fc25907345a478de274966c6e8fcb
Agus Friastana <friastana@gmail.com>
2020-03-03 08:44:52 +0800

    add column status note, updated at, updated by

--- VolcomStockOpname/FormStockTakeCancel.vb
+++ VolcomStockOpname/FormStockTakeCancel.vb
@@ -13,30 +13,34 @@
         Dim id_st_trans As String = ""
         Dim table As String = ""
         Dim column As String = ""
+        Dim column_by As String = ""
+        Dim column_update As String = ""
 
         If FormMain.XtraTabbedMdiManager1.SelectedPage.MdiChild.Name = "FormStockTake" Then
+            table = "tb_st_trans"
+            column = "id_st_trans"
+            column_by = "st_trans_updated_by"
+            column_update = "st_trans_updated"
+
             If FormStockTake.XTCStockTake.SelectedTabPageIndex = 0 Then
                 id_st_trans = FormStockTake.GVScan.GetFocusedRowCellValue("id_st_trans").ToString
-                table = "tb_st_trans"
-                column = "id_st_trans"
             ElseIf FormStockTake.XTCStockTake.SelectedTabPageIndex = 1 Then
                 id_st_trans = FormStockTake.GVCombine.GetFocusedRowCellValue("id_st_trans").ToString
-                table = "tb_st_trans"
-                column = "id_st_trans"
             End If
         ElseIf FormMain.XtraTabbedMdiManager1.SelectedPage.MdiChild.Name = "FormVerStockTake" Then
+            table = "tb_st_trans_ver"
+            column = "id_st_trans_ver"
+            column_by = "st_trans_ver_updated_by"
+            column_update = "st_trans_ver_updated"
+
             If FormVerStockTake.XTCStockTake.SelectedTabPageIndex = 0 Then
                 id_st_trans = FormVerStockTake.GVScan.GetFocusedRowCellValue("id_st_trans_ver").ToString
-                table = "tb_st_trans_ver"
-                column = "id_st_trans_ver"
             ElseIf FormVerStockTake.XTCStockTake.SelectedTabPageIndex = 1 Then
                 id_st_trans = FormVerStockTake.GVCombine.GetFocusedRowCellValue("id_st_trans_ver").ToString
-                table = "tb_st_trans_ver"
-                column = "id_st_trans_ver"
             End If
         End If
 
-        execute_non_query("UPDATE " + table + " SET id_report_status = 5, report_status_note = '" + addSlashes(MemoNote.Text) + "' WHERE " + column + " = " + id_st_trans, True, "", "", "", "")
+        execute_non_query("UPDATE " + table + " SET id_report_status = 5, report_status_note = '" + addSlashes(MemoNote.Text) + "', " + column_by + " = " + id_user + ", " + column_update + " = NOW() WHERE " + column + " = " + id_st_trans, True, "", "", "", "")
 
         If FormMain.XtraTabbedMdiManager1.SelectedPage.MdiChild.Name = "FormStockTake" Then
             If FormStockTake.XTCStockTake.SelectedTabPageIndex = 0 Then
